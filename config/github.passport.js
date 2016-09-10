e8cb3077205237147deb6cc6b696fa53f2f286b3
joshpaulchan <joshpaulchan@gmail.com>
2016-09-10 16:00:45 -0400

    Add passport configs

---
+++ config/github.passport.js
@@ -0,0 +1,37 @@
+var models = require('../models');
+var Strategy = require('passport-github').Strategy;
+var bcrypt = require('bcrypt');
+
+module.exports = new Strategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.HOST + "/auth/github/cb"
+  },
+  function(username, password, done) {
+  // Find the user with the given username
+  models.User.findOne({ username }, function (err, user) {
+    // if there's an error, finish trying to authenticate (auth failed)
+    if (err) {
+      console.error(err);
+      done(err);
+      return;
+    }
+    // if no user present, auth failed
+    if (!user) {
+      console.log(user);
+      done(null, false, { message: 'Incorrect username.' });
+      return;
+    }
+    // if passwords do not match, auth failed
+    bcrypt.compare(password, user.password, function(err, res) {
+      // res == true
+      if (!res) {
+        done(null, false, { message: 'Incorrect password.' });
+        return;
+      }
+      // auth has has succeeded
+      done(null, user);
+      return;
+    });
+  });
+});
