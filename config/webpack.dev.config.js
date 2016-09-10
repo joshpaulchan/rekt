aadce96b7e4718cf8174be418de12a82a1dd77ab
joshpaulchan <joshpaulchan@gmail.com>
2016-09-10 16:44:29 -0400

    Finally get webpack config working

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,24 +1,28 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// console.log("dirname", __dirname);
+// console.log("joined dirname", path.join(__dirname, '../public/dist'));
+// console.log("joined dirname for public", path.join(__dirname, '../public'));
+
 module.exports = {
   devtool: 'eval',
   entry: [
     './client/index'
   ],
   output: {
-    path: '../public/dist',
+    path: path.join(__dirname, '../public/dist'),
     filename: 'bundle.js',
-    publicPath: '/dist'
+    publicPath: '/dist/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'client')
+      include: path.join(__dirname, '../client')
     }]
   }
 };
