ab494630ccd0283dd1fc7f065285f9ec99dcc8ba
joshpaulchan <joshpaulchan@gmail.com>
2016-09-27 19:54:35 -0400

    Add unsetUser functionality

--- client/actions/index.js
+++ client/actions/index.js
@@ -7,22 +7,41 @@
 const LOG_IN = '@@auth/SUBMIT_LOGIN_FORM';
 const ADD = '@@test/ADD';
 const SET_USER = '@@app/SET_USER';
+const UNSET_USER = '@@app/UNSET_USER';
 
 // ACTIONS /////////////////////////////////////////////////////////////////////
 
 // `setUser`
 // 
-// This function sets the current globallogged-in user to the application, if
+// This function sets the current global logged-in user to the application, if
 // any
 // 
 // @params: user<Object>: the user object that has just logged in
-// @returns: null
+// @returns: action<Object>:
+//    type<String> : actions.SET_USER type
+//    user<Object> : user object
 function setUser(user) {
   // TODO: replace with a dispatch
   // this.setState({ user });
   return {
     type: SET_USER,
     user
+  }
+}
+
+// `unsetUser`
+// 
+// This function unsets the current global logged-in user to the application, if
+// any
+// 
+// @params: null
+// @returns: action<Object>:
+//    type<String> : actions.UNSET_USER type
+function unsetUser(user) {
+  // TODO: replace with a dispatch
+  // this.setState({ user });
+  return {
+    type: UNSET_USER
   }
 }
 
@@ -48,11 +67,13 @@ module.exports = {
   types: {
     LOG_IN,
     ADD,
-    SET_USER
+    SET_USER,
+    UNSET_USER
   },
   actions: {
     logIn,
     add,
-    setUser
+    setUser,
+    unsetUser
   }
 }
--- client/reducers/app.js
+++ client/reducers/app.js
@@ -8,14 +8,15 @@ const initialState = {
 module.exports = (state, action) => {
   if (!state) return initialState
   
-  // console.log("[reducer] state", state);
-  // console.log("[reducer] action", action);
-  
   switch (action.type) {
     
     case types.SET_USER:
       return Object.assign({}, state, {
-        user: action.user
+        user: action.user,
+      });
+    case types.UNSET_USER:
+      return Object.assign({}, state, {
+        user: null,
       });
     default:
       return state
